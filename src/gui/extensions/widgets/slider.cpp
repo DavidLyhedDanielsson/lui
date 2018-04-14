085fb60ad75ed618ed7888880d26e15cf62dba4c
David Lyhed Danielsson <davidlyheddanielsson@gmail.com>
2018-04-14 22:51:44 +0200

    Added "text_format" to sliders
    If the user wants to print the value of a slider, the text_format
    property can be set to a string with standard printf formatting.
    However, the only valid values are %f and %p, where %p is used to show
    the slider's value as a percentage.
    
    For example, the following would show "Value: 25":
        {
            type = "slider
            , value = 25
            , value_min = 0
            , value_max = 50
            , value_increment = 1
            , text_format = "Value: %f"
        }
    and the following would show "Value: 50%":
        {
            type = "slider
            , value = 25
            , value_min = 0
            , value_max = 50
            , value_increment = 1
            , text_format = "Value: %p%%"
        }
    
    Simple error checking is performed, and the text will print INVALID if
    the text string is invalid. It must contain one and only one instance of
    either %p or %f to be considered valid.
    
    Max length is currently set to 255 characters, which I feel should be
    enough for most applications.

--- src/gui/extensions/widgets/slider.cpp
+++ src/gui/extensions/widgets/slider.cpp
@@ -14,6 +14,7 @@
  * }
 */
 #include <stdint.h>
+#include <cctype>
 #include <lua5.1/lua.hpp>
 #include <gui/guielement.h>
 #include <gui/lib.h>
@@ -38,11 +39,37 @@ struct Data
     float thumbSize;
     float pos;
 
+    bool percent;
+
     Scrollbar::DIRECTION direction;
 
     bool mouseDown;
     bool mouseInside;
+
+    char* textFormat;
 };
+const static int TEXT_BUFFER_LENGTH = 256; // This is only the length of a temporary buffer, 256 should be fine
+
+void UpdateText(Widget* widget) {
+    Data* data = (Data*)widget->data;
+    // Reset these before updating so createText can be called
+    widget->offsetData.vertex = 8;
+    widget->offsetData.index = 12;
+    widget->offsetData.vertexBegin = 8;
+
+    char buffer[TEXT_BUFFER_LENGTH];
+    if(data->percent) {
+        float percent = (data->currentValue - data->minValue) / (data->maxValue - data->minValue) * 100.0f;
+        sprintf(buffer, data->textFormat, percent);
+    } else {
+        sprintf(buffer, data->textFormat, data->currentValue);
+    }
+    auto vertexOffset = widget->offsetData.vertex;
+    functions->createText(widget, buffer, 255, 255, 255, 255);
+    AdjustText(widget->vertices + vertexOffset, strlen(buffer), (Origin)(Origin::CENTER_VERTICAL | Origin::CENTER_HORIZONTAL), widget->bounds.x + widget->bounds.width * 0.5f, widget->bounds.y + widget->bounds.height * 0.5f);
+    widget->vertexCount = 8 + strlen(buffer) * 4;
+    widget->indexCount = 12 + strlen(buffer) * 6;
+}
 
 extern "C"
 {
@@ -59,6 +86,7 @@ extern "C"
         data.mouseInside = false;
         data.thumbSize = 0.0f;
         data.pos = 0.0f;
+        data.percent = false;
 
         BackgroundColor::Parse(state, &data.backgroundColor, defaults);
         Scrollbar::Parse(state
@@ -70,7 +98,83 @@ extern "C"
                             , &data.thumbColor
                             , defaults);
 
-        QUAD_ALLOC(widget, 2);
+        int maxStringLength = 0;
+        if(FieldExists(state, "text_format")) {
+            const char* textFormat = nullptr;
+            char buffer[TEXT_BUFFER_LENGTH];
+            textFormat = lua_tostring(state, -1);
+            int length = strlen(textFormat);
+            bool foundFormat = false;
+            bool valid = true;
+            // Make sure there is one and only one %f\%p
+            for(int i = 0; i < length + 1 && valid; ++i) {
+                if(textFormat[i] == '%') {
+                    if(i + 1 < length) {
+                        buffer[i] = textFormat[i];
+                        if(textFormat[i + 1] == 'f') {
+                            if(foundFormat) {
+                                valid = false; // Multiple %
+                            }
+                            foundFormat = true;
+                        } else if(textFormat[i + 1] == '%') {
+                            buffer[i + 1] = textFormat[i + 1];
+                            ++i;
+                        } else {
+                            if(foundFormat) {
+                                valid = false;
+                            }
+                            foundFormat = true;
+                            for(; i < length; ++i) {
+                                valid = false;
+                                if(std::isalpha(textFormat[i])) {
+                                    if(textFormat[i] == 'p') {
+                                        // Replace %p with %f, since value is still a float
+                                        data.percent = true;
+                                        buffer[i] = 'f';
+                                        valid = true;
+                                        break;
+                                    } else if(textFormat[i] == 'f') {
+                                        buffer[i] = 'f';
+                                        valid = true;
+                                        break;
+                                    } else {
+                                        // Only p or f is valid
+                                        break;
+                                    }
+                                } else {
+                                    buffer[i] = textFormat[i];
+                                }
+                            }
+                        }
+                    } else {
+                        // Invalid, trailing %
+                        valid = false;
+                        break;
+                    }
+                } else {
+                    buffer[i] = textFormat[i];
+                }
+            }
+
+            if(!valid || !foundFormat) {
+                strcpy(buffer, "INVALID");
+            }
+
+            data.textFormat = (char*)functions->memalloc(length + 1);
+            strcpy(data.textFormat, buffer);
+
+            if(data.percent) {
+                maxStringLength = snprintf(nullptr, 0, data.textFormat, 100.0f);
+            } else {
+                maxStringLength = std::max(snprintf(nullptr, 0, data.textFormat, data.minValue), snprintf(nullptr, 0, data.textFormat, data.maxValue));
+            }
+
+            lua_pop(state, 1);
+        } else {
+            data.textFormat = nullptr;
+        }
+
+        QUAD_ALLOC(widget, 2 + std::min(maxStringLength, TEXT_BUFFER_LENGTH - 1));
         
         widget->data = (Data*)functions->memalloc(sizeof(Data));
         *(Data*)widget->data = data;
@@ -113,6 +217,9 @@ extern "C"
                                         , &data->thumbSize
                                         , data->thumbColor);
         }
+
+        if(data->textFormat)
+            UpdateText(widget);
     }
 
     void OnEnter(GUI::Widget* widget, lua_State* state)
@@ -162,6 +269,9 @@ extern "C"
             widget->vertices[6].y = data->pos + data->thumbSize;
             widget->vertices[7].y = data->pos;
         }
+
+        if(data->textFormat)
+            UpdateText(widget);
 
         widget->modified = true;
     }
@@ -239,5 +349,12 @@ extern "C"
         }
 
         return false;
+    }
+
+    void Destroy(void* dataPtr, lua_State* state)
+    {
+        Data* data = (Data*)dataPtr;
+        if(data->textFormat)
+            functions->memdealloc(data->textFormat);
     }
 }
